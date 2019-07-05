fc1b3baaada0819ebf64b6c3ce12896e701cecf8
ISTLOJA V <ISTLOJA V@ISTLOJA14>
2019-07-05 09:32:16 -0500

    Tarea

---
+++ PracticaPOOfrancelpineda/src/Tarea/Generaciones.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tarea;
+
+/**
+ *
+ * @author ISTLOJA V
+ */
+public class Generaciones {
+    public static void main(String[] args) {
+ {
+
+        int año = 1950;
+        int nacimiento = 50;
+         
+        switch(año){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+                nacimiento =18;
+                break;
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+                nacimiento =32;
+                break;
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+            case 38:
+                
+                
+ }
+ }
+    }
+}
+            
